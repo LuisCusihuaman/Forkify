18427d3587c7e4f05ad9a925b1572c9c6d109b79
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2019-10-07 11:15:28 -0300

    Created search object, where queries are made and will have the states

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import Search from "./models/Search";
+
+/* Global stage of the app
+    -Search object
+    - Current recipe object
+    - Shopping list object
+    - Liked recipes
+ */
+
+const controlSearch = () => {};
