4fa7e9aa2b37263ea3384116d976055628219565
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2019-10-08 14:22:37 -0300

    Fixed limitRecipeTitle, now display the title correctly

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -4,15 +4,14 @@ export const clearInput = () => (elements.searchInput.value = "");
 export const clearResult = () => (elements.searchResList.innerHTML = "");
 
 const limitRecipeTitle = (title, limit = 17) => {
+	if (title.length < limit) return title;
 	const newTitle = [];
-	if (title.length > limit) {
-		title.split(" ").reduce((acc, curr) => {
-			if (acc + curr.length <= limit) {
-				newTitle.push(curr);
-			}
-			return acc + curr.length;
-		}, 0);
-	}
+	title.split(" ").reduce((acc, curr) => {
+		if (acc + curr.length <= limit) {
+			newTitle.push(curr);
+		}
+		return acc + curr.length;
+	}, 0);	
 	return `${newTitle.join(" ")} ...`;
 };
 const renderRecipe = recipe => {
@@ -23,8 +22,8 @@ const renderRecipe = recipe => {
                             <img src="${recipe.image_url}" alt="${recipe.title}">
                         </figure>
                         <div class="results__data">
-                            <h4 class="results__name">${recipe.title}</h4>
-                            <p class="results__author">${recipe.publisher}</p>
+							<h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
+							<p class="results__author">${recipe.publisher}</p>
                         </div>
                     </a>
                 </li>
