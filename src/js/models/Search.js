c17f618a2f5ded0309a72d4d3de676f1800a82d4
Luis Cusihuaman <43934057+LuisCusihuaman@users.noreply.github.com>
2020-03-28 13:51:40 -0300

    Fix bug in search.js

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -8,8 +8,6 @@ export default class Search {
 		//old key = 13b636854a1030f7b75474d792b69850
 		try {
 			const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
-
-			);
 			this.result = res.data.recipes;
 		} catch (error) {
 			alert(error);
